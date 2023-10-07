be3e2c7a7d39f51fb1806b297581de8c370f7528
Lucas S. Corte <corte1994.lc@gmail.com>
2023-10-07 20:15:20 -0300

    refactor: use error helper

--- src/presentation/routers/login-router.js
+++ src/presentation/routers/login-router.js
@@ -1,16 +1,17 @@
+import { HTTPResponse } from '../helpers/http-response.js'
+
 export class LoginRouter {
   route (httpRequest) {
     if (!httpRequest || !httpRequest.body) {
-      return {
-        statusCode: 500
-      }
+      return HTTPResponse.serverError()
     }
 
     const { email, password } = httpRequest.body
-    if (!email || !password) {
-      return {
-        statusCode: 400
-      }
+    if (!email) {
+      return HTTPResponse.badRequest('email')
+    }
+    if (!password) {
+      return HTTPResponse.badRequest('password')
     }
   }
 }
