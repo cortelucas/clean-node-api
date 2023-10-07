74c40915d21b3b621ab4fd5d2fe22d712e91dba6
Lucas S. Corte <corte1994.lc@gmail.com>
2023-10-07 19:38:04 -0300

    test: ensure LoginRouter returns 400 if no email is provided

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,15 @@
+import { LoginRouter } from './login-router.js'
+
+describe('Login Router', () => {
+  it('should return 400 if no email is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
