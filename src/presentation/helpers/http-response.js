4fa0cb6f3460e98449ca1984a97fe21843662099
Lucas S. Corte <corte1994.lc@gmail.com>
2023-10-07 20:14:26 -0300

    feat: implement HTTPResponse service

---
+++ src/presentation/helpers/http-response.js
@@ -0,0 +1,16 @@
+import { MissingParamError } from '../../utils/errors/missing-param-error.js'
+
+export class HTTPResponse {
+  static badRequest (paramName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(paramName)
+    }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500
+    }
+  }
+}
