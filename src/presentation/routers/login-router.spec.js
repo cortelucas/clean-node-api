057ab3040f862d1ea2a628ed6090d8f1cde7c26e
Lucas S. Corte <corte1994.lc@gmail.com>
2023-10-09 20:42:33 -0300

    test: ensure LoginRouter returns 500 if an invalid AuthUseCase is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -99,4 +99,19 @@ describe('Login Router', () => {
 
     expect(httpResponse.statusCode).toBe(500)
   })
+
+  it('should return 500 if no AuthUseCase is provided', () => {
+    class AuthUseCaseSpy {}
+
+    const sut = new LoginRouter(new AuthUseCaseSpy())
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(500)
+  })
 })
