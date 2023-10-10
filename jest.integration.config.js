1390490a8d9a968d9f77087e4ad4645e04a03d51
Lucas S. Corte <corte1994.lc@gmail.com>
2023-10-09 22:22:23 -0300

    config: create custom configs to unit and integration tests

---
+++ jest.integration.config.js
@@ -0,0 +1,4 @@
+import config from './jest.config.js'
+config.testMatch = ['**/*.test.js']
+
+export default config
---
+++ jest.unit.config.js
@@ -0,0 +1,4 @@
+import config from './jest.config.js'
+config.testMatch = ['**/*.spec.js']
+
+export default config
